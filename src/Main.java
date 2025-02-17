a25c413cea53195eeea5d8cb8955bfa84254b077
Evan <spanglem@uc.edu>
2025-02-16 23:17:25 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner in= new Scanner(System.in);
+        System.out.print("Please enter your age: ");
+        int age = in.nextInt();
+        if (age >= 21) {
+            System.out.println("You get a paper wristband.");
+        }
+    }
+}
